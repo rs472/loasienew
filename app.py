9d776253a8e1fd0f1ea05455cbd363d821282005
Rafael Silva <63508411+rs472@users.noreply.github.com>
2026-09-15 23:23:12 -0300

    Add files via upload
    fixed

--- app.py
+++ app.py
@@ -526,6 +526,5 @@ def verify_2fa():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-    #app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
